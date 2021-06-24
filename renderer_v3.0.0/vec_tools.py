712fa348f00852b79f6ff0bcd5051e0c33676ffe
DoCloudCompute <dodo.sc1508@gmail.com>
2021-06-24 12:49:32 +0200

    Added vec3_add to vector tools

--- renderer_v3.0.0/vec_tools.py
+++ renderer_v3.0.0/vec_tools.py
@@ -8,6 +8,11 @@ def negative(a):
             - a[1],
             - a[2])
 
+def vec3_add(a, b):
+    return (a[0]+b[0],
+            a[1]+b[1],
+            a[2]+b[2])
+
 def det3(a, b, c):
     return a[0]*b[1]*c[2] + b[0]*c[1]*a[2] + c[0]*a[1]*b[2] - c[0]*b[1]*a[2] - b[0]*a[1]*c[2] - a[0]*c[1]*b[2]
 
