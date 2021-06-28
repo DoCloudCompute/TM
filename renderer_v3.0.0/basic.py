e72e53bf159e522b990ba10bc5178cb0ec5468a3
DoCloudCompute <dodo.sc1508@gmail.com>
2021-06-28 07:37:07 +0200

    Added reflection

--- renderer_v3.0.0/basic.py
+++ renderer_v3.0.0/basic.py
@@ -20,48 +20,59 @@ def gen_ray(pixel_x, pixel_y, screen, viewer_origin):
          screen[0][2] + screen[1][2]*pixel_x + screen[2][2]*pixel_y)
 
     d = vec_tools.vec3(viewer_origin, S) # directing vector
-    d = vec_tools.unit_vec3(d)
+    d = vec_tools.unit3(d)
     return viewer_origin, d
 
 
-def gen_triangle_vectors(triangle_vertices):
+def gen_triangle_vectors(triangle_vertices, color):
+    # color are BGR (Blue, Green, Red)
     # transform triangles to triangles_vec (E1 = B - A | E2 = C - A)
     triangles_vec = []
     for tri in triangle_vertices:
         A = tri[0]
         B = tri[1]
         C = tri[2]
-        normal = tri[3]
+        normal = vec_tools.unit3(tri[3])
 
         E1 = vec_tools.vec3(B, A)
         E2 = vec_tools.vec3(C, A)
 
-        tri_vec_element = [A, E1, E2]
+        tri_vec_element = [A, E1, E2, normal, color]
         triangles_vec.append(tri_vec_element)
 
     return triangles_vec
 
+def gen_bounce_ray(normal, origin, d):
+    # https://math.stackexchange.com/questions/13261/how-to-get-a-reflection-vector
+    d_dot_n = vec_tools.dot3(d, normal)
+    d2 = vec_tools.mult3_scalar(d, 2)
+    right_product = vec_tools.mult3_scalar(d2,  d_dot_n)
 
-def get_intersection(triangles_vec, ray):
+    bounce_d = vec_tools.sub3(d, right_product)
+
+    return origin, bounce_d
+
+def get_intersection(triangles_vec, ray, max_reflection, reflection_depth = 0):
     pix_RGB = (0,0,0) # set color
+
     O, d = ray # unpack origin and directing vector
 
     # will be used to check for closest triangle
     smallest_t = None
 
     for tri in triangles_vec:
-        V, E1, E2 = tri
+        #print(tri)
+        V, E1, E2, normal, color = tri
         VO = vec_tools.vec3(O, V)
 
         cross1 = vec_tools.cross(E1, E2)
         neg_d = vec_tools.negative(d)
         invdet = -1/(vec_tools.dot3(d, cross1))
-        #invdet = 1/(vec_tools.det3(neg_d, E1, E2))
 
         t = abs(vec_tools.dot3(VO, cross1) * invdet)
         if not smallest_t: smallest_t = t
 
-        if t <= smallest_t:
+        if t <= smallest_t and t > 0:
             cross2 = vec_tools.cross(VO, neg_d)
 
             neg_E2 = vec_tools.negative(E2)
@@ -70,7 +81,19 @@ def get_intersection(triangles_vec, ray):
 
             if u+v <= 1 and u >= 0 and v >= 0:
                 smallest_t = t
-                pix_RGB = (255, 255*u, 255*v)
+                pix_RGB = color
+
+                # if the desired trace is not yet complete, keep reflecting
+                if reflection_depth < max_reflection:
+                    # origin of bounce ray
+                    intersection_coords = (V[0] + E1[0]*u + E2[0]*v,
+                                           V[1] + E1[1]*u + E2[1]*v,
+                                           V[2] + E1[2]*u + E2[2]*v)
+
+                    bounce_ray = gen_bounce_ray(normal, intersection_coords, d)
+
+                    reflection = get_intersection(triangles_vec, bounce_ray, max_reflection, reflection_depth+1)
+                    pix_RGB = vec_tools.add3(pix_RGB, reflection)
 
     return pix_RGB
 
@@ -85,16 +108,18 @@ def main():
     # create the image output buffer
     res_image = np.zeros((resolution[1], resolution[0], 3), dtype=np.uint8)
 
-    triangle_vertices = read_stl("../utils/icosphere_X_offset.stl")
-    #triangle_vertices = [[(2,0,-1), (3,1,1), (2,-1,1)]]
-    triangles_vec = gen_triangle_vectors(triangle_vertices)
+    triangle_vertices = read_stl("../utils/icosphere_red_right.stl")
+    triangles_vec = gen_triangle_vectors(triangle_vertices, (0, 0, 255))
+
+    triangle_vertices = read_stl("../utils/icosphere_yellow_left.stl")
+    triangles_vec = triangles_vec + gen_triangle_vectors(triangle_vertices, (0, 255, 255))
 
     screen = make_screen(2330, resolution)
 
-    for pixel_x in range(resolution[0]):
+    for pixel_x in range(300, resolution[0]):
         for pixel_y in range(resolution[1]):
             ray = gen_ray(pixel_x, pixel_y, screen, viewer_origin)
-            pix_RGB = get_intersection(triangles_vec, ray)
+            pix_RGB = get_intersection(triangles_vec, ray, max_reflection=1)
 
             if pix_RGB != (0,0,0):
                 res_image[pixel_y, pixel_x] = pix_RGB
--- renderer_v3.0.0/vec_tools.py
+++ renderer_v3.0.0/vec_tools.py
@@ -8,10 +8,15 @@ def negative(a):
             - a[1],
             - a[2])
 
-def vec3_add(a, b):
+def add3(a, b):
     return (a[0]+b[0],
             a[1]+b[1],
             a[2]+b[2])
+
+def sub3(a, b):
+    return (a[0]-b[0],
+            a[1]-b[1],
+            a[2]-b[2])
 
 def det3(a, b, c):
     return a[0]*b[1]*c[2] + b[0]*c[1]*a[2] + c[0]*a[1]*b[2] - c[0]*b[1]*a[2] - b[0]*a[1]*c[2] - a[0]*c[1]*b[2]
@@ -22,11 +27,16 @@ def det2(a, b):
 def dot3(a, b):
     return a[0]*b[0] + a[1]*b[1] + a[2]*b[2]
 
-def dot3x3(a, b, c):
-    return a[0]*b[0]*c[0] + a[1]*b[1]*c[1] + a[2]*b[2]*c[2]
-
 def dot2(a, b):
     return a[0]*b[0] + a[1]*b[1]
+
+def mult3(a, b):
+    return (a[0]*b[0],
+            a[1]*b[1],
+            a[2]*b[2])
+
+def mult3_scalar(a, b):
+    return mult3(a, [b]*3)
 
 def vec3(a, b):
     return (a[0] - b[0],
@@ -37,10 +47,10 @@ def vec2(a, b):
     return (a[0] - b[0],
             a[1] - b[1])
 
-def vec3_norm(a):
+def norm3(a):
     return (a[0]**2 + a[1]**2 + a[2]**2)**0.5
 
-def unit_vec3(a):
-    return (a[0]/vec3_norm(a),
-            a[1]/vec3_norm(a),
-            a[2]/vec3_norm(a))
+def unit3(a):
+    return (a[0]/norm3(a),
+            a[1]/norm3(a),
+            a[2]/norm3(a))
