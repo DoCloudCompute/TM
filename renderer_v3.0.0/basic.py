223b2552e558c1a4cc72681411132baeae0b6f57
DoCloudCompute <dodo.sc1508@gmail.com>
2021-08-01 16:54:02 +0200

    Avoid parallel division by zero

--- renderer_v3.0.0/basic.py
+++ renderer_v3.0.0/basic.py
@@ -70,10 +70,12 @@ def get_intersection(triangles_vec, ray, max_reflection, reflection_depth = 0):
 
         cross1 = vec_tools.cross(E1, E2) # that's the normal
         neg_d = vec_tools.negative(d)
-        invdet = -1/(vec_tools.dot3(d, cross1))
+        pre_invdet = vec_tools.dot3(d, cross1)
 
         t = abs(vec_tools.dot3(VO, cross1) * invdet)
         if not smallest_t: smallest_t = t
+        if abs(pre_invdet) < 1e-5:
+            continue
 
         if t <= smallest_t and t > 0:
             cross2 = vec_tools.cross(VO, neg_d)
