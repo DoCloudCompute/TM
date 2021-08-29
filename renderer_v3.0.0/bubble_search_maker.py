1c82af7cb5891b138d3399d42fe145ae936f64c8
DoCloudCompute <dodo.sc1508@gmail.com>
2021-08-29 13:11:14 +0200

    folder re-structure

--- renderer_v3.0.0/bubble_search_maker.py
+++ renderer_v3.0.0/bubble_search_maker.py
@@ -1,17 +1,89 @@
 import vec_tools
 from STL_reader import read_stl
 
-def step1(triangles):
-    """
-    this shit will make an average of the location of the vertices and find the center or something
-    its called a centroid
-    """
-    for face in triangles:
-        A = face[0]
-        B = face[1]
-        C = face[2]
 
-        # average out these coords
-        # find vertex farthest from center
+def find_farthest_val(list_of_points, target):
+    dist_lst = []
+    for point in list_of_points:
+        distance_squared = vec_tools.points_separation_sq(point, target)
+        dist_lst.append(distance_squared)
+
+    return max(dist_lst)
+
+
+def find_closest_id(list_of_bubbles, target):
+    smallest_dist = None
+
+    for bubble in list_of_bubbles:
+        point = bubble[0]
+        distance_squared = vec_tools.points_separation_sq(point, target)
+
+        if distance_squared != 0:
+            if smallest_dist == None:
+                smallest_dist = distance_squared
+
+
+
+
+def gen_triangle_vectors(triangle_vertices, color):
+    # color are BGR (Blue, Green, Red)
+    # transform triangles to triangles_vec (E1 = B - A | E2 = C - A)
+    triangles_vec = []
+    for tri in triangle_vertices:
+        A = tri[0]
+        B = tri[1]
+        C = tri[2]
+
+        centroid = vec_tools.centroid3(A, B, C)
+
+        radius_squared = find_farthest_val([A, B, C], centroid)
+
+        E1 = vec_tools.sub3(B, A)
+        E2 = vec_tools.sub3(C, A)
+
+        normal = vec_tools.cross(E1, E2)
+
+        tri_vec_element = [A, E1, E2, normal, color]
+        tri_bubble = [centroid, radius_squared, [A, E1, E2, tri_vec_element]
+        triangles_vec.append(tri_bubble)
+
+    return triangles_vec
+
+
+def group_triangles_lv1(triangles_vec):
+    """
+    The idea with this function is that it make a bubble with 3 triangles innit olright
+    """
+
+    triangle_groups = []
+    for bubble_id, bubble in enumerate(triangles_vec):
+        centroid_1 = bubble[0]
+        radius_squared_1 = bubble[1]
+
+
+
+
+
+        for rpt in range(2):
+            closest_dist_squared = 0
+            for i, test_triangle in enumerate(triangles_vec):
+                test_centroid = test_triangle[4]
+
+                dist_vec = vec_tools.sub3(test_centroid, centroid)
+                dist_squared = vec_tools.norm3_sq(dist_vec)
+                if dist_squared < closest_dist_squared:
+                    closest_dist_squared = dist_squared
+
+
 
 triangles = read_stl("STLs/refl_plane.stl")
+
+
+"""
+farthest_dist_squared = 0
+for vertex in face:
+    centroid_vertex_vec = vec_tools.sub3(vertex, centroid)
+    dist_squared = vec_tools.norm3_sq(centroid_vertex_vec)
+    if dist_squared > farthest_dist_squared:
+        farthest_dist_squared = dist_squared
+"""
---
+++ renderer_v4.0.0/vec_tools.py
@@ -0,0 +1,92 @@
+def cross(a, b):
+    return (a[1]*b[2] - a[2]*b[1],
+            a[2]*b[0] - a[0]*b[2],
+            a[0]*b[1] - a[1]*b[0])
+
+def negative(a):
+    return (- a[0],
+            - a[1],
+            - a[2])
+
+def add3(a, b):
+    return (a[0]+b[0],
+            a[1]+b[1],
+            a[2]+b[2])
+
+def sub3(a, b):
+    return (a[0]-b[0],
+            a[1]-b[1],
+            a[2]-b[2])
+
+def div3(a,b):
+    return (a[0]/b[0],
+            a[1]/b[1],
+            a[2]/b[2])
+
+def div3_scalar(a,b):
+    return (a[0]/b,
+            a[1]/b,
+            a[2]/b)
+
+def det3(a, b, c):
+    return a[0]*b[1]*c[2] + b[0]*c[1]*a[2] + c[0]*a[1]*b[2] - c[0]*b[1]*a[2] - b[0]*a[1]*c[2] - a[0]*c[1]*b[2]
+
+def det2(a, b):
+    return a[0]*b[1] - b[0]*a[1]
+
+def dot3(a, b):
+    return a[0]*b[0] + a[1]*b[1] + a[2]*b[2]
+
+def dot2(a, b):
+    return a[0]*b[0] + a[1]*b[1]
+
+def mult3(a, b):
+    return (a[0]*b[0],
+            a[1]*b[1],
+            a[2]*b[2])
+
+def mult3_scalar(a, b):
+    return mult3(a, [b]*3)
+
+def vec3(a, b):
+    return (a[0] - b[0],
+            a[1] - b[1],
+            a[2] - b[2])
+
+def vec2(a, b):
+    return (a[0] - b[0],
+            a[1] - b[1])
+
+def norm3(a):
+    return (a[0]**2 + a[1]**2 + a[2]**2)**0.5
+
+def norm3_sq(a):
+    return (a[0]**2 + a[1]**2 + a[2]**2)
+
+def points_separation_sq(a, b):
+    vec_BA = sub3(a, b)
+    return norm3_sq(vec_BA)
+
+def norm2(a):
+    return (a[0]**2 + a[1]**2)**0.5
+
+def unit3(a):
+    n = norm3(a)
+    return (a[0]/n,
+            a[1]/n,
+            a[2]/n)
+
+def centroid3(a, b, c):
+    return ((a[0] + b[0] + c[0]) / 3,
+            (a[1] + b[1] + c[1]) / 3,
+            (a[2] + b[2] + c[2]) / 3)
+
+def mixRGB(a, b, f=0.5):
+    # fix f between 0 and 1
+    f = min([1, f])
+    f = max([0, f])
+
+    opp_f = 1-f
+    return (a[0]*f + b[0]*opp_f,
+            a[1]*f + b[1]*opp_f,
+            a[2]*f + b[2]*opp_f)
